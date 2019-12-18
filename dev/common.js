731b89189202fb13ee802a1a6bda365ceb014b58
ALREstevam <a166348@dac.unicamp.br>
2019-12-17 22:38:40 -0300

    adding find max/min

--- dev/common.js
+++ dev/common.js
@@ -1,14 +1,46 @@
 import uuid from 'uuid/v1'
 
-export function setHasOnly(set, element){
+export function setHasOnly(set, element) {
     return set.size === 1 && set.has(element)
 }
 
-export function replaceAll(str, change, to){
+export function replaceAll(str, change, to) {
     return str.toString().split(change).join(to)
 }
 
-export function getUuid(){
+export function getUuid() {
     return replaceAll(uuid(), '-', '')
 }
 
+export const findIndex = {
+    max: (array, minimum = -Infinity) => {
+        let maxValue = minimum
+        let maxIndex = undefined
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > maxValue) {
+                maxValue = array[i]
+                maxIndex = i
+            }
+        }
+        return {
+            value: maxValue,
+            index: maxIndex
+        }
+    },
+    min: (array) => {
+        let minValue = -Infinity
+        let minIndex = undefined
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i]
+                minIndex = i
+            }
+        }
+        return {
+            value: minValue,
+            index: minIndex
+        }
+    },
+}
